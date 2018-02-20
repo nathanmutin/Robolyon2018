bb23abd969c1a5abfd03f8336482d9dc17fcf29d
Gnu-Bricoleur <sylvain.migaud@gmail.com>
2018-02-20 11:10:31 +0100

    branche robot2018

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,65 +1,57 @@
-
 #include <iostream>
+#include <memory>
 #include <string>
-
-#include <Joystick.h>
-#include <VictorSP.h>
-#include <PWMVictorSPX.h>
-#include <Encoder.h>
-#include <DriverStation.h>
-#include <CameraServer.h>
-#include <DoubleSolenoid.h>
-#include "WPILib.h"
-
 #include <IterativeRobot.h>
 #include <LiveWindow/LiveWindow.h>
-#include <SmartDashboard/SendableChooser.h>
-#include <SmartDashboard/SmartDashboard.h>
+#include <VictorSP.h>
+#include <Joystick.h>
+#include <CameraServer.h>
+#include <Encoder.h>
+#include "WPILib.h"
+#include <thread>
 
-class Robot : public frc::IterativeRobot
-{
+#include "Commands/ExampleCommand.h"
+#include "Commands/MyAutoCommand.h"
+
+class Robot : public frc:: IterativeRobot {
+
 public:
 	void RobotInit()
 	{
-		Joystick1 = new Joystick(0);
-
 		CimDroit1 = new VictorSP(0);
 		CimDroit2 = new VictorSP(1);
 		CimGauche1 = new VictorSP(2);
 		CimGauche2 = new VictorSP(3);
-
-		EncodeurDroit = new Encoder(0, 1, false, Encoder::EncodingType::k4X);
-		EncodeurGauche = new Encoder(2, 3, false, Encoder::EncodingType::k4X);
-
-		DoubleSolenoid1 = new DoubleSolenoid(0, 1);
-		DoubleSolenoid1->Set(frc::DoubleSolenoid::Value::kForward);
-		std::cout << "*************** VITESSE 1 ACTIVEE ***************" << std::endl;
-		vitesseBallShifter = false;
 
 		CimGauche1->Set(0);
 		CimGauche2->Set(0);
 		CimDroit1->Set(0);
 		CimDroit2->Set(0);
 
+		EncodeurDroit = new Encoder (0,1, false, Encoder::EncodingType::k4X);
+		EncodeurDroit->Reset();
+		EncodeurDroit->SetReverseDirection(false);
+
+		EncodeurGauche = new Encoder (2,3, false, Encoder::EncodingType::k4X);
+		EncodeurGauche->Reset();
+		EncodeurGauche->SetReverseDirection(true);
+
 		CameraServer::GetInstance()->StartAutomaticCapture(0);
 		CameraServer::GetInstance()->SetSize(0);
 	}
 
-
-	void AutonomousInit() override
+	void DisabledInit()
 	{
-		/*std::string gameData = frc::DriverStation::GetInstance().GetGameSpecificMessage();
-		std::string L ="L";
 
-		if(gameData[0] == L[0])
-		{
-			//Code gauche
-		}
-		else
-		{
-			//Code droit
-		}*/
+	}
 
+	void DisabledPeriodic()
+	{
+
+	}
+
+	void AutonomousInit()
+	{
 
 	}
 
@@ -70,75 +62,68 @@ public:
 
 	void TeleopInit()
 	{
+		pulsesParTours = 360;
+		distanceParTour = 48;
+		distance_a_parcourir = 0;
+		integrale = 0;
+		/*iMin = ?;
+		iMax = ?;*/
 
+		kP = 2;
+		kI = 0;
+		kD = 0;
 	}
 
 	void TeleopPeriodic()
 	{
-		//############ Contrôle du moteur ############
-		y = Joystick1->GetY();
-		z = Joystick1->GetZ();
+		distance_parcourue = ((EncodeurDroit->Get() + EncodeurGauche->Get()) /2) /7.5;
+		erreur = (distance_a_parcourir - distance_parcourue) / distance_a_parcourir;
 
-		if(y < 0.2 && y > -0.2)
+		proportionelle = kP * erreur;
+
+		integrale += erreur;
+		/*if(integrale > iMax)
 		{
-			y = 0;
+			integrale = iMax;
 		}
-		if(z < 0.2 && z > -0.2)
+		else if(integrale < iMin)
 		{
-			z = 0;
-		}
+			integrale = iMin;
+		}*/
+		integrale = kI * integrale;
 
-		vitesseDroite = -y - 0.5 * z;
-		vitesseGauche = y - 0.5 * z;
+		derivee = kD * (erreur - erreurPrecedente);
+		erreurPrecedente = erreur;
 
-		CimDroit1->Set(vitesseDroite);
-		CimDroit2->Set(vitesseDroite);
-		CimGauche1->Set(vitesseGauche);
-		CimGauche2->Set(vitesseGauche);
+		vitesse = proportionelle + integrale + derivee;
 
-		//############ Changement de vitesse ############
-		if(Joystick1->GetTriggerPressed())
-		{
-			if(vitesseBallShifter)
-			{
-				DoubleSolenoid1->Set(frc::DoubleSolenoid::Value::kForward);
-				std::cout << "*************** VITESSE 1 ACTIVEE ***************" << std::endl;
-				vitesseBallShifter = false;
-			}
-			else
-			{
-				DoubleSolenoid1->Set(frc::DoubleSolenoid::Value::kReverse);
-				std::cout << "*************** VITESSE 2 ACTIVEE ***************" << std::endl;
-				vitesseBallShifter = true;
-			}
-		}
+		CimGauche1->Set(-vitesse);
+		CimGauche2->Set(-vitesse);
+		CimDroit1->Set(vitesse);
+		CimDroit2->Set(vitesse);
 
-		//############ Valeurs encodeurs ############
-		//std::cout << "Encodeur droit : " << EncodeurDroit->Get() << std::endl;
-		//std::cout << "Encodeur gauche : " << EncodeurGauche->Get() << std::endl;
+		std::cout << distance_parcourue << std::endl;
 	}
-
 	void TestPeriodic()
 	{
+
 	}
 
-private:
-	Joystick* Joystick1 = 0;
 
+private:
 	VictorSP* CimDroit1 = 0;
 	VictorSP* CimDroit2 = 0;
 	VictorSP* CimGauche1 = 0;
 	VictorSP* CimGauche2 = 0;
 
-	Encoder* EncodeurDroit = 0;
-	Encoder* EncodeurGauche = 0;
+	Encoder* EncodeurDroit;
+	Encoder* EncodeurGauche;
 
-	DoubleSolenoid* DoubleSolenoid1 = 0;
-
-	bool vitesseBallShifter;
-
-	double x, y, z;
-	double vitesseDroite, vitesseGauche;
+	double pulsesParTours, distanceParTour;
+	double distance_a_parcourir, distance_parcourue, erreur, erreurPrecedente, vitesse;
+	double proportionelle, kP;
+	double integrale, iMin, iMax, kI;
+	double derivee, kD;
 };
 
 START_ROBOT_CLASS(Robot)
